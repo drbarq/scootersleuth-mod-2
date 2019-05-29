04ceffa5ed6f68c988c3c40194cfa9ac7cb473ed
drbarq <J.Tustin@gmail.com>
2019-05-29 08:30:13 -0600

    worked on scooter class, to create methods to grab the closests scooters nad create the new table

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,2 +1,18 @@
 class Bird < ApplicationRecord
+    def self.closest
+        # pass in value of number of scooters needed
+        Bird.first(5)
+    end 
+
+    def self.merge_table # takes the first 5 entries in the returned results and creates new entries in the aggregated table
+        Bird.closest.each do |bird_scoot|
+            Scooter.create(
+                company: lime_scoot.company, 
+                latitude: lime_scoot.latitude, 
+                longitude: lime_scoot.longitude, 
+                battery_level: lime_scoot.battery_level
+                )
+        end 
+    end 
+
 end
