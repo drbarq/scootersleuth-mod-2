ec9f6798843598d32f95aa45e9b8fd1dd32e4812
ahare14 <alexanderhare14@gmail.com>
2019-05-29 20:13:37 -0600

    map rendering do not merge

--- db/migrate/20190529211904_create_locations.rb
+++ db/migrate/20190529211904_create_locations.rb
@@ -2,8 +2,9 @@ class CreateLocations < ActiveRecord::Migration[5.2]
   def change
     create_table :locations do |t|
       t.string :address
-      t.float :latitude
-      t.float :longitude
+      t.decimal :latitude
+      t.decimal :longitude
+
       t.timestamps
     end
   end
