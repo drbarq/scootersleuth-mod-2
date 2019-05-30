78050c3fb46ae414f9196155b1c81feb829b68e0
Christopher Tero <christopher.tero@gmail.com>
2019-05-30 14:14:54 -0600

    added spec files for testing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,17 +1,17 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  describe User do
-    let(:attributes) do
-      {
-        first_name: "John",
-        last_name: "Doe",
-        email: "someemail@email.to"
-      }
-    end
-
-    it "is considered valid" do
-      expect(User.new(attributes)).to be_valid
-    end
-  end
+  # describe User do
+  #   let(:attributes) do
+  #     {
+  #       first_name: "John",
+  #       last_name: "Doe",
+  #       email: "someemail@email.to"
+  #     }
+  #   end
+  #
+  #   it "is considered valid" do
+  #     expect(User.new(attributes)).to be_valid
+  #   end
+  # end
 end
