a783bc8432f83ce79656944802892a3929d7ffdb
drbarq <J.Tustin@gmail.com>
2019-05-30 10:04:48 -0600

    removed spin from forms and tables

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -74,6 +74,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :bird, :lime, :spin, :jump)
+      params.require(:user).permit(:first_name, :last_name, :email, :bird, :lime, :jump)
     end
 end
