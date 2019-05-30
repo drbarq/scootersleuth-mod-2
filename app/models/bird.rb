b1202366b49642a93978a90a250d4bce39fd0b62
drbarq <J.Tustin@gmail.com>
2019-05-29 19:34:37 -0600

    bird model updated to hit api call

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -6,9 +6,11 @@ class Bird < ApplicationRecord
     geocoded_by [:latitude, :longitude]
 
 
-    def self.get_latest #api call to get the latest bird.  need tp update with latest location 
-        
-        uri = URI.parse("https://api.bird.co/bird/nearby?latitude=39.758596&longitude=-105.007265&radius=1000")
+    def self.get_latest #api call to get the latest bird.
+        latitude = Location.last.latitude
+        longitude = Location.last.longitude
+
+        uri = URI.parse("https://api.bird.co/bird/nearby?latitude=#{latitude}&longitude=#{longitude}&radius=1000")
         request = Net::HTTP::Get.new(uri)
         request["Accept"] = "*/*"
         request["App-Version"] = "4.24.5"
@@ -17,7 +19,7 @@ class Bird < ApplicationRecord
         request["Connection"] = "keep-alive"
         request["Device-Id"] = "c259a2e6-0754-481f-98a1-bf945cd0ba03"
         request["Host"] = "api.bird.co"
-        request["Location"] = "{\"latitude\":39.758292,\"longitude\":-105.007622,\"altitude\":500,\"accuracy\":100,\"speed\":-1,\"heading\":-1}"
+        request["Location"] = "{\"latitude\":#{latitude},\"longitude\":#{longitude},\"altitude\":500,\"accuracy\":100,\"speed\":-1,\"heading\":-1}"
         request["Postman-Token"] = "ddbf2231-43b6-4309-aab7-45bf9f7f69fa,ceaf7fcf-c5e8-40b7-b9c5-21d16bd2746b"
         request["User-Agent"] = "PostmanRuntime/7.11.0"
         request["Cookie"] = "__cfduid=ddc33f4510477e1aa82f855b71ed815091558999853"
@@ -25,23 +27,17 @@ class Bird < ApplicationRecord
         req_options = {
           use_ssl: uri.scheme == "https",
         }
-        
+       
         response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
           http.request(request)
         end
 
-        @@jsonresponse = JSON.parse(response.body)["birds"]
+        jsonresponse = JSON.parse(response.body)["birds"]
 
-        # this creates the json on the response.body 
-        # need to parse through it 
-        
-        # response.code
-        # response.body
-        # byebug
     end 
 
-    def self.seed_scooter
-        @@jsonresponse.each do |scooter|
+    def self.create_scooter
+        Bird.get_latest.each do |scooter|
             Bird.create(company: "Bird",
                 latitude: scooter["location"]["latitude"], 
                 longitude: scooter["location"]["longitude"], 
@@ -49,11 +45,10 @@ class Bird < ApplicationRecord
         end 
     end 
 
-
-
     def self.closest
-        # pass in value of number of scooters needed
-        Bird.first(5)
+        Bird.create_scooter
+
+        Bird.near([Location.last.latitude, Location.last.longitude]).first(5)
     end 
 
     def self.merge_table # takes the first 5 entries in the returned results and creates new entries in the aggregated table
